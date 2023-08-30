b1fc4bff218871144ef812034aa52edf215892c9
kcc <g2c1010@naver.com>
2023-08-30 10:26:31 +0900

    feat: init project

---
+++ OurTicket/src/main/java/Ko/OurTicket/OurTicketApplication.java
@@ -0,0 +1,13 @@
+package Ko.OurTicket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurTicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OurTicketApplication.class, args);
+	}
+
+}
---
+++ OurTicket/src/test/java/Ko/OurTicket/OurTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package Ko.OurTicket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurTicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
