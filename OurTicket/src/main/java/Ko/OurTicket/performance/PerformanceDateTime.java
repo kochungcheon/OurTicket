4e7ff69377fec9f2137e6683cd7d343ea52186b0
kcc <g2c1010@naver.com>
2023-09-01 15:42:25 +0900

    refactor: dateTime

--- OurTicket/src/main/java/Ko/OurTicket/performance/PerformanceDateTime.java
+++ OurTicket/src/main/java/Ko/OurTicket/performance/PerformanceDateTime.java
@@ -7,15 +7,15 @@ import lombok.NoArgsConstructor;
 @Embeddable
 @NoArgsConstructor
 public class PerformanceDateTime {
-    private LocalDateTime performanceStartDate;
-    private LocalDateTime performanceEndDate;
+    private LocalDateTime performanceStartDateTime;
+    private LocalDateTime performanceEndDateTimeTime;
 
-    public PerformanceDateTime(final LocalDateTime performanceStartDate, final LocalDateTime performanceEndDate) {
-        this.performanceStartDate = performanceStartDate;
-        this.performanceEndDate = performanceEndDate;
+    public PerformanceDateTime(final LocalDateTime performanceStartDate, final LocalDateTime performanceEndDateTime) {
+        this.performanceStartDateTime = performanceStartDate;
+        this.performanceEndDateTimeTime = performanceEndDateTime;
     }
 
-    public static PerformanceDateTime of(final LocalDateTime performanceStartDate, final LocalDateTime performanceEndDate){
-        return new PerformanceDateTime(performanceStartDate, performanceEndDate);
+    public static PerformanceDateTime of(final LocalDateTime performanceStartDate, final LocalDateTime performanceEndDateTime){
+        return new PerformanceDateTime(performanceStartDate, performanceEndDateTime);
     }
 }
