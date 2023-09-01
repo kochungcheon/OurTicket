12a73388bfa3c906f377bf5c564a8671834da008
kcc <g2c1010@naver.com>
2023-09-01 15:07:39 +0900

    refactor: add Grade description

--- OurTicket/src/main/java/Ko/OurTicket/Ticket/Grade.java
+++ OurTicket/src/main/java/Ko/OurTicket/Ticket/Grade.java
@@ -1,7 +1,15 @@
 package Ko.OurTicket.Ticket;
 
 public enum Grade {
-    VIP,
-    R,
-    S
+    VIP("최고 등급 좌석"),
+    R("중간 등급 좌석"),
+    S("일반 등급 좌석");
+
+    private String description;
+    private Grade(String description){
+        this.description = description;
+    }
+    public String getDescription(){
+        return this.description;
+    }
 }
