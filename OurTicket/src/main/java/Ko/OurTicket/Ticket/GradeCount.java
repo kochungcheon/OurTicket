dda17526a21e24df10c541f25cf55468f7cfefb4
kcc <g2c1010@naver.com>
2023-09-01 15:27:47 +0900

    chore: remove import

--- OurTicket/src/main/java/Ko/OurTicket/Ticket/GradeCount.java
+++ OurTicket/src/main/java/Ko/OurTicket/Ticket/GradeCount.java
@@ -1,8 +1,6 @@
 package Ko.OurTicket.Ticket;
 
-import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 
 public record GradeCount(Map<Grade, Integer> gradeCountMap) {
     public GradeCount(Map<Grade, Integer> gradeCountMap) {
--- OurTicket/src/main/java/Ko/OurTicket/Ticket/TicketService.java
+++ OurTicket/src/main/java/Ko/OurTicket/Ticket/TicketService.java
@@ -2,7 +2,6 @@ package Ko.OurTicket.Ticket;
 
 import Ko.OurTicket.performance.Performance;
 import Ko.OurTicket.performance.PerformanceRepository;
-import jakarta.persistence.criteria.CriteriaBuilder.In;
 import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
