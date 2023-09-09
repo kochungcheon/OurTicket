2c4ff8c973f5513124eb17704009a1c70827e5cd
kcc <g2c1010@naver.com>
2023-09-09 13:36:20 +0900

    refactor: RESTful URL api/ticket -> api/tickets

--- OurTicket/src/main/java/ko/ourticket/ticket/TicketController.java
+++ OurTicket/src/main/java/ko/ourticket/ticket/TicketController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/ticket")
+@RequestMapping("/api/tickets")
 @RequiredArgsConstructor
 public class TicketController {
     private TicketService ticketService;
