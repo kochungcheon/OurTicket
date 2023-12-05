1374aaca199ad5f79da75932d8852570b656f4b7
kcc <g2c1010@naver.com>
2023-12-05 16:47:11 +0900

    chore: @리퀘스트 바디 추가

--- OurTicket/src/main/java/ko/ourticket/purchase/PurchaseController.java
+++ OurTicket/src/main/java/ko/ourticket/purchase/PurchaseController.java
@@ -2,6 +2,7 @@ package ko.ourticket.purchase;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
@@ -12,7 +13,7 @@ public class PurchaseController {
 	private final PurchaseTicketLock purchaseTicketLock;
 
 	@PostMapping("/api/tickets/ticketReserve")
-	public ResponseEntity<String> reserveTicket(PurchaseRequest purchaseRequest) {
+	public ResponseEntity<String> reserveTicket(@RequestBody PurchaseRequest purchaseRequest) {
 		purchaseTicketLock.purchaseInOrder(
 			purchaseRequest.nickName(),
 			purchaseRequest.ticketId(),
