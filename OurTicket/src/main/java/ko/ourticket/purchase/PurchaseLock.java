ffca8259121b9ad35b39989856af44ed24350b1d
kcc <g2c1010@naver.com>
2023-12-12 21:09:12 +0900

    feat: 스케줄러 기능 추가

--- OurTicket/src/main/java/ko/ourticket/purchase/PurchaseLock.java
+++ OurTicket/src/main/java/ko/ourticket/purchase/PurchaseLock.java
@@ -1,23 +1,46 @@
 package ko.ourticket.purchase;
 
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.ReentrantLock;
 
 import org.springframework.stereotype.Component;
 
-import lombok.RequiredArgsConstructor;
-
 @Component
-@RequiredArgsConstructor
 public class PurchaseLock implements Lockable {
-	private ConcurrentHashMap<Long, ReentrantLock> lock = new ConcurrentHashMap<>();
+	private final ConcurrentHashMap<Long, ReentrantLock> lockMap = new ConcurrentHashMap<>();
+	private final ConcurrentHashMap<Long, Long> timestampMap = new ConcurrentHashMap<>();
+	private final ScheduledExecutorService ticketLockChecker = Executors.newScheduledThreadPool(1);
+
+	private PurchaseLock() {
+		ticketLockChecker.scheduleAtFixedRate(this::cleanUp, 0, 6, TimeUnit.HOURS);
+	}
+
 	public void lock(Long ticketId, Runnable runnable) {
-		System.out.println(ticketId);
-		lock.computeIfAbsent(ticketId, k -> new ReentrantLock(true)).lock();
+		ReentrantLock lock = lockMap.computeIfAbsent(ticketId, k -> new ReentrantLock(true));
+		timestampMap.put(ticketId, System.currentTimeMillis());
+		lock.lock();
 		try {
 			runnable.run();
 		} finally {
-			lock.get(ticketId).unlock();
-		};
+			lock.unlock();
+			timestampMap.put(ticketId, System.currentTimeMillis());
+		}
+	}
+
+	private void cleanUp() {
+		long threshold = System.currentTimeMillis() - TimeUnit.HOURS.toMillis(6);
+		for (Long ticketId : timestampMap.keySet()) {
+			if (timestampMap.get(ticketId) < threshold) {
+				ReentrantLock lock = lockMap.get(ticketId);
+				if (lock != null && !lock.isLocked()) {
+					lockMap.remove(ticketId);
+					timestampMap.remove(ticketId);
+				}
+			}
+		}
 	}
 }
+
